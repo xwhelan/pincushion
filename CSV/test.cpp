7f70a575fdf355afc94e1c1291f1af7941dd19c0
Peter Whelan <xwhelan@gmail.com>
2021-12-31 18:57:58 -0500

    Test all CSV behavior for lines

--- CSV/test.cpp
+++ CSV/test.cpp
@@ -1,6 +1,30 @@
 #include <catch2/catch_test_macros.hpp>
+#include <string>
+#include <vector>
+#include "csv.cpp"
 
-TEST_CASE( "Hello, Catch2!", "[hello]" ) {
-    REQUIRE( 1 == 1 );
+TEST_CASE( "Unquoted CSV line", "[unquoted]" ) {
+    const auto unquoted_line = "foo,bar,baz";
+    auto row = pincushion::readCSVRow(unquoted_line);
+    REQUIRE( row[0] == "foo" );
 }
 
+TEST_CASE( "Quoted CSV line", "[quoted]" ) {
+    const auto quoted_line = "\"foo\",bar,\"baz\"";
+    auto row = pincushion::readCSVRow(quoted_line);
+    REQUIRE( row[0] == "foo");
+    REQUIRE( row[1] == "bar");
+    REQUIRE( row[2] == "baz" );
+}
+
+TEST_CASE( "Quoted CSV with escaped quote character", "[escaped]" ) {
+    // This one is tricky due to multiple layers of escapes
+    /* NB: example code from Stack Overflow does quotes inside quoted fields
+     * as doubled-up quotes
+     * Refer to https://datatracker.ietf.org/doc/html/rfc4180
+     */
+    const auto escaped_line = "foo,\"bar \"\"baz\",bing";
+    auto row = pincushion::readCSVRow(escaped_line);
+    const std::vector<std::string> expected = { "foo", "bar \"baz", "bing" };
+    REQUIRE( row == expected );
+}
