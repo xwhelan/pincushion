eb8985c1decb4590765805314d70952b2c7a2314
Peter Whelan <xwhelan@gmail.com>
2021-12-31 19:55:41 -0500

    Add Boost command line parsing foundations

--- src/csv2json.cpp
+++ src/csv2json.cpp
@@ -1,8 +1,25 @@
 #include <iostream>
 #include <boost/program_options.hpp>
 
+using namespace std;
 namespace po = boost::program_options;
 
-int main(){
+int main(int argc, char* argv[]) {
+    // Declare the supported options.
+    po::options_description desc("Allowed options");
+    desc.add_options()
+        ("help", "produce help message")
+    ;
+
+    po::variables_map vm;
+    po::store(po::parse_command_line(argc, argv, desc), vm);
+    po::notify(vm);
+
+    if (vm.count("help")) {
+        cerr << desc << "\n";
+    return 1;
+}
+
     std::cout << "Hello, from pincushion!\n";
+    return 0;
 }
